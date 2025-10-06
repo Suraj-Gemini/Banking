575f36be9266ab4681f3d96eba1c8ce5f368d8f7
Suraj-Gemini <Suraj.Kumar1@Geminisolutions.com>
2025-10-06 10:47:26 +0530

    Minor Changes

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,21 +1,12 @@
-
 import { useAppDispatch, useAppSelector } from '../store/hooks'
 import { toggleTheme } from '../features/theme/themeSlice'
 
-import { Button } from '@mui/material';
-
-export default function ThemeToggle() {
-  const dispatch = useAppDispatch();
-  const mode = useAppSelector(s => s.theme.mode);
-
+export default function ThemeToggle(){
+  const dispatch = useAppDispatch()
+  const mode = useAppSelector(s=>s.theme.mode)
   return (
-    <Button
-      variant="outlined"
-      color="inherit"
-      onClick={() => dispatch(toggleTheme())}
-      aria-label="Toggle theme"
-    >
+    <button className="btn secondary" onClick={()=>dispatch(toggleTheme())} aria-label="Toggle theme">
       {mode === 'light' ? 'Light' : 'Dark'}
-    </Button>
-  );
+    </button>
+  )
 }
